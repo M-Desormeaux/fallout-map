044d58d3fc1a10feafa2f79b36727cc26cc71a0e
Michael Desormeaux <mdesormeaux96@gmail.com>
2024-05-01 14:20:14 -0500

    disable ssr

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import dynamic from "next/dynamic";
 import Image from "next/image";
 
-const Map = dynamic(() => import("../components/Map"));
+const Map = dynamic(() => import("../components/Map"), { ssr: false });
 
 export default function Home() {
   return (
